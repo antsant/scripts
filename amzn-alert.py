d317d62987939c579a6e4e006078ecebef9a0d6a
EC2 Default User <ec2-user@ip-172-31-37-153.us-west-2.compute.internal>
2015-10-23 06:06:23 +0000

    Added DynamoDB to check if the recipient has already been notified for the search term

--- amzn-alert.py
+++ amzn-alert.py
@@ -1,4 +1,6 @@
 #/usr/bin/python
+from boto.dynamodb2.fields import HashKey, RangeKey
+from boto.dynamodb2.table import Table
 from bs4 import BeautifulSoup
 
 import argparse
@@ -12,6 +14,13 @@ parser.add_argument('--email', metavar="ADDRESS", required=True)
 args = vars(parser.parse_args())
 search_term = args['name']
 recipient = args['email']
+
+searches_notified_table = Table('SearchesNotified', schema=[
+                HashKey('Recipient'),
+                RangeKey('SearchTerm')
+        ])
+is_notified = searches_notified_table.get_item(Recipient=recipient, SearchTerm=search_term)
+print(is_notified)
 
 search_url = "http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=" + search_term + "+"
 
